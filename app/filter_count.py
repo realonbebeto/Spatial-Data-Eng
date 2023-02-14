36066e6f0d3e8d9017209f013705af6f312f7dda
realonbebeto <projobs254@gmail.com>
2023-02-14 14:24:23 +0300

    ETL update

---
+++ app/filter_count.py
@@ -0,0 +1,33 @@
+from sqlalchemy import create_engine, text
+from config import settings
+
+DATABASE_URL = f'postgresql+psycopg2://{settings.database_username}:{settings.database_password}@{settings.database_host}:{settings.database_port}/{settings.database_name}'
+
+engine = create_engine(DATABASE_URL)
+
+
+def filter_count(region: str, sex: str, hiv: str = 'yes') -> int:
+    """
+    filter_count runs to count the number of records in the database based 
+    on two features who have ever heard of AIDS
+
+    Params:
+    region (string): area of interest for the survey e.g. 'Northern, 'Central'
+    sex (string): gender of interest e.g. 'male' or 'female'
+    hiv (string): if person is hiv aware e.g. 'yes' or 'no'
+
+    Returns:
+    count(int): the number of records based on region and sex
+    """
+    query = text("""SELECT COUNT(*) 
+                    FROM ug_dhs
+                    WHERE sex=:sex AND hiv_aware=:hiv AND name=:region""").bindparams(region=region, sex=sex, hiv=hiv)
+    count = engine.execute(query).fetchall()[0][0]
+    return count
+
+
+if __name__ == '__main__':
+    region = 'Northern'
+    sex = 'male'
+    hiv = 'yes'
+    print(filter_count(region, sex, hiv))
